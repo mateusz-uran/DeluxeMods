3552c737b074f937dd1ed33b0ad937762db94f21
mateusz-uran <mateusz.uranowski@onet.pl>
2025-06-24 16:43:09 +0200

    feat: call api for mods, start creating layout for main page with last 10 mods

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,51 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import api from "@/utils/api";
 
 export default function Home() {
+  const [mods, setMods] = useState([]);
+
+  useEffect(() => {
+    async function fetchMods() {
+      try {
+        const response = await api.get("/api/mod");
+        setMods(response.data);
+      } catch (error) {
+        console.error("Failed to return last ten mods!");
+      }
+    }
+
+    fetchMods()
+  }, []);
+
   return (
     <div className={styles.page}>
       <main>
-        <h2>Główna Strona</h2>
-        <p>
-          Donec non eros urna. Mauris id tellus sagittis, lacinia sem id, ornare
-          dui. Vestibulum volutpat rutrum dolor ut consequat. In eget ante ac
-          ipsum auctor tempus at vel est. Ut laoreet eu lectus et placerat.
-          Suspendisse orci nunc, gravida sed semper et, maximus vitae nulla. Sed
-          tellus turpis, lobortis a pulvinar nec, vulputate at nisi. Vestibulum
-          vel mauris id quam fermentum scelerisque. Nunc porta velit in nisl
-          posuere malesuada. Sed quis ipsum iaculis, pulvinar lectus ut,
-          imperdiet sapien. Integer nec accumsan mi. Maecenas hendrerit sapien
-          vitae dignissim laoreet. Curabitur vulputate, magna ac gravida
-          lacinia, purus libero tincidunt augue, a dapibus mi arcu vel sem.
-          Aliquam tincidunt est vulputate leo luctus pulvinar vel quis nunc.
-          Nunc nec aliquet augue, sodales blandit enim. Etiam porttitor lectus
-          id nisi fermentum molestie. Suspendisse ac lorem mi. Cras posuere
-          porttitor mauris, ut feugiat elit imperdiet sit amet. Aliquam eu lorem
-          eu justo tristique gravida. Sed ac volutpat augue. Aenean vitae
-          laoreet dolor. Pellentesque urna nunc, rutrum eget vehicula quis,
-          iaculis pharetra neque. Sed porta erat orci, quis efficitur ex
-          ultrices vel. Etiam tellus leo, pharetra nec lorem ut, semper
-          malesuada leo. Etiam vel vehicula dui, nec congue risus. Integer
-          tempus sem eget scelerisque pharetra. Morbi convallis elit et est
-          consectetur, in varius enim tempor. Pellentesque in mauris mollis est
-          ultrices suscipit eget sed erat. Suspendisse vulputate convallis
-          imperdiet. Phasellus id neque vitae nibh placerat porta at non nulla.
-          Morbi eleifend pretium elit, eu hendrerit quam ullamcorper sit amet.
-          Duis quis luctus sapien. Donec gravida nisl arcu, nec placerat nisi
-          porttitor ac. Sed volutpat est quis ligula mollis mattis. Ut vitae
-          dignissim lorem. Etiam porttitor risus non augue ullamcorper blandit.
-          Aenean quis tortor a ante vulputate pellentesque eget sed nulla.
-          Nullam venenatis neque sed scelerisque iaculis. Sed sit amet placerat
-          leo. Quisque facilisis nunc metus, sed semper ex lobortis et.
-          Phasellus nec bibendum nunc. Suspendisse facilisis, diam id interdum
-          convallis, mauris urna venenatis nisi, et malesuada mauris purus ac
-          est. Sed ac libero tincidunt quam dapibus dapibus. Vestibulum sed urna
-          non leo sagittis laoreet. Suspendisse eget eros vitae sapien iaculis
-          efficitur. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Duis semper nulla id egestas bibendum.
-          Pellentesque egestas erat quis leo venenatis mattis. Phasellus nec
-          maximus libero.
-        </p>
+        {mods.map((singleMod => (
+          <div key={singleMod.slug}>
+            <h3>{singleMod.name}</h3>
+            <img src={singleMod.previewPhoto} alt="mod" />
+          </div>
+        )))}
       </main>
+      <aside>nawigacja</aside>
     </div>
   );
 }
