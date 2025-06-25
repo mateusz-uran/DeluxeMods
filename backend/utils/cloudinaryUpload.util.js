9e5ddb21c2b25a91cb36e1716f6440a74bed1e38
mateusz-uran <mateusz.uranowski@onet.pl>
2025-06-25 11:44:36 +0200

    feat: resize image before uplaod to cloudinary

--- backend/utils/cloudinaryUpload.util.js
+++ backend/utils/cloudinaryUpload.util.js
@@ -3,7 +3,11 @@ import cloudinary from "../config/cloudinary.js";
 export function uploadImageToCloudinary(buffer) {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: "mods/previews", allowed_formats: ["jpg","jpeg","png"] },
+      {
+        folder: "mods/previews",
+        allowed_formats: ["jpg", "jpeg", "png"],
+        transformation: [{ width: 600, crop: "scale" }],
+      },
       (error, result) => {
         if (error) return reject(error);
         resolve(result.secure_url);
