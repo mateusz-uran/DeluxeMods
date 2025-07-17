f37354fab9defc6038f4addcf6c583f2540bc4b3
mateusz-uran <mateusz.uranowski@onet.pl>
2025-07-17 14:55:43 +0200

    test: finish tests for review service

--- backend/service/review.service.js
+++ backend/service/review.service.js
@@ -2,16 +2,21 @@ import Review from "../models/Review.js";
 import { checkIfModExists, updateModReviewId } from "./mod.service.js";
 import { validateUserById } from "./user.service.js";
 
-export async function createReview({ author, text, modId }) {
-  if (!(await checkIfModExists(modId))) {
+export async function createReview(
+  { author, text, modId },
+  checkMod = checkIfModExists,
+  updateMod = updateModReviewId,
+  validateUser = validateUserById
+) {
+  if (!(await checkMod(modId))) {
     throw new Error("Mod not found!");
   }
 
-  const user = await validateUserById(author);
+  const user = await validateUser(author);
 
   const review = await Review.create({ author: user._id, text });
 
-  await updateModReviewId(modId, review._id);
+  await updateMod(modId, review._id);
 
   return review;
 }
