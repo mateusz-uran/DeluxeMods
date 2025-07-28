5804312c8d0fd870971bae42ff8fb9e83a97f707
mateusz-uran <mateusz.uranowski@onet.pl>
2025-07-28 16:56:45 +0200

    refactor: update auth util and config variables

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,13 +7,17 @@ interface Config {
     nodeEnv: string;
     mongoUri: string;
     frontendUri: string;
+    tokenSecret: string;
+    refreshSecret: string;
 }
 
 const config: Config = {
     port: Number(process.env.APP_PORT) || 5000,
     nodeEnv: process.env.NODE_ENV || 'development',
     mongoUri: process.env.MONGO_DB_URI || '',
-    frontendUri: process.env.FRONTEND_URL || 'http://localhost:3000'
+    frontendUri: process.env.FRONTEND_URL || 'http://localhost:3000',
+    tokenSecret: process.env.TOKEN_SECRET || '',
+    refreshSecret: process.env.REFRESH_SECRET || ''
 }
 
 export default config
