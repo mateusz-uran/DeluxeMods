1534d4fdcfdec22bb0d56b9435d3678ae0e855e2
mateusz-uran <mateusz.uranowski@onet.pl>
2025-07-30 17:12:31 +0200

    refactor: add routers to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import config from './config/env';
 import { errorHandler } from './middleware/error';
+import auhtRouter from './routes/auth.router';
+import userRouter from "./routes/user.router";
+import reviewRouter from "./routes/review.router";
+import modRouter from "./routes/mod.router";
 
 const app = express();
 
@@ -15,6 +19,11 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/", auhtRouter)
+app.use("/user", userRouter);
+app.use("/review", reviewRouter)
+app.use("/mod", modRouter)
 
 app.use(errorHandler);
 
